a916400eef06a4a3e5593e75e8b6aa34d5784e0b
Kelliryan12 <60245183+Kelliryan12@users.noreply.github.com>
2020-02-13 18:11:19 -0500

    Finalized Birthday Wishes Program
    Added lblBox and txtName to input the users name. Added Enter Box to output Message Box. Added lstBox to output Happy Birthday Song. Added btnClear and btnExit.

---
+++ Birthday Wishes/Birthday Wishes/BirthdayWishes.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BirthdayWishes
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnClick = New System.Windows.Forms.Button()
+        Me.lstOut = New System.Windows.Forms.ListBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(32, 43)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(278, 25)
+        Me.lblName.TabIndex = 0
+        Me.lblName.Text = "Please Enter Your Name "
+        '
+        'txtName
+        '
+        Me.txtName.BackColor = System.Drawing.Color.LightGray
+        Me.txtName.Font = New System.Drawing.Font("Book Antiqua", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(36, 94)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(140, 27)
+        Me.txtName.TabIndex = 1
+        '
+        'btnClick
+        '
+        Me.btnClick.BackColor = System.Drawing.Color.CornflowerBlue
+        Me.btnClick.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClick.ForeColor = System.Drawing.Color.White
+        Me.btnClick.Location = New System.Drawing.Point(36, 148)
+        Me.btnClick.Name = "btnClick"
+        Me.btnClick.Size = New System.Drawing.Size(95, 29)
+        Me.btnClick.TabIndex = 2
+        Me.btnClick.Text = "Enter"
+        Me.btnClick.UseVisualStyleBackColor = False
+        '
+        'lstOut
+        '
+        Me.lstOut.FormattingEnabled = True
+        Me.lstOut.Location = New System.Drawing.Point(36, 212)
+        Me.lstOut.Name = "lstOut"
+        Me.lstOut.Size = New System.Drawing.Size(246, 134)
+        Me.lstOut.TabIndex = 3
+        '
+        'btnClear
+        '
+        Me.btnClear.Font = New System.Drawing.Font("Book Antiqua", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(36, 377)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Book Antiqua", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(151, 377)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Birthday_Wishes.My.Resources.Resources.Balloons
+        Me.PictureBox1.Location = New System.Drawing.Point(383, 148)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(218, 203)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'BirthdayWishes
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Ivory
+        Me.ClientSize = New System.Drawing.Size(677, 476)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.lstOut)
+        Me.Controls.Add(Me.btnClick)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblName)
+        Me.Name = "BirthdayWishes"
+        Me.Text = "Birthday Wishes"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblName As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnClick As Button
+    Friend WithEvents lstOut As ListBox
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Birthday Wishes/Birthday Wishes/BirthdayWishes.vb
@@ -0,0 +1,26 @@
+﻿Public Class BirthdayWishes
+    Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
+        'Show message box 
+        MessageBox.Show("Thank you! " & txtName.Text)
+
+        'Print Happy Birthday Song to list box
+        lstOut.Items.Add("Happy Birthday to You, Happy Birthday to You,")
+        lstOut.Items.Add("Happy Birthday to You,")
+        lstOut.Items.Add("Happy Birthday to You!")
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        'Clear txtName Value 
+        txtName.Clear()
+        lstOut.Items.Clear()
+
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'When user exits pogram
+        Application.Exit()
+
+    End Sub
+End Class
